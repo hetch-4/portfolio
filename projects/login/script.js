843267e4cc1985bcf66628ddc536e6fd837d44b9
hetch-4 <humphreydola@gmail.com>
2025-02-28 12:14:41 +0300

    live pages

---
+++ projects/login/script.js
@@ -0,0 +1,8 @@
+function showSignup(){
+    document.getElementById("signin").classList.add("hidden");
+    document.getElementById("signup").classList.remove("hidden");
+}
+function showLogin(){
+    document.getElementById("signup").classList.add("hidden");
+    document.getElementById("signin").classList.remove("hidden");
+}
