a17b8e2f2ce6775d74cfdcf30a8fbf8d83c1e196
hetch-4 <humphreydola@gmail.com>
2025-03-01 16:38:04 +0300

    removed this file

--- projects/login/script.js
+++
@@ -1,8 +0,0 @@
-function showSignup(){
-    document.getElementById("signin").classList.add("hidden");
-    document.getElementById("signup").classList.remove("hidden");
-}
-function showLogin(){
-    document.getElementById("signup").classList.add("hidden");
-    document.getElementById("signin").classList.remove("hidden");
-}
